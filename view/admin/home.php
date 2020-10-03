4ee81c3bcba3c5331040c162d8efc257a8f11ba8
jaider Bernal <jaider.bernal90@gmail.com>
2020-10-03 13:27:08 -0500

    Menu terminado V1

--- view/admin/home.php
+++ view/admin/home.php
@@ -18,78 +18,15 @@
 </head>
 <body class="scrollbar-light-blue">
    <!--Navbar-->
-    <nav class="navbar navbar-dark menu-bar fixed-top">
-        <!-- Navbar brand -->
-        <img src="../../img/Brandcolor-little-menu.png" alt="" class="logo-menu">
-        <main class="navbar-toggler d-flex">
-            <div class="group-cont navbar-toggler">
-            <!-- Message and user -->
-                <ul class="navbar-nav ml-auto nav-flex-icons">
-                    <p class="text-dark pt-4 text-menu  d-none d-lg-block ">PEPITO PEREZ</p>
-                    <li class="nav-item">
-                    <a class="nav-link waves-effect waves-light" id="btnn">
-                        <i class="fas fa-envelope icon-message px-3"></i>
-                    </a>
-                    </li>
-                    <li class="nav-item avatar dropdown img-icon px-2  d-none d-lg-block">    
-                        <img src="../../img/user.png" width="42" height="42" class="rounded-circle z-depth-0"
-                        alt="avatar image">
-                    </li>
-                </ul>
-            </div>
-            <!-- Collapse button -->
-            <button class="navbar-toggler third-button text-dark" type="button" data-toggle="collapse" data-target="#navbarSupportedContent22"
-            aria-controls="navbarSupportedContent22" aria-expanded="false" aria-label="Toggle navigation">
-            <div class="animated-icon3"><span></span><span></span><span></span></div>
-            </button>
-        </main>
-        <!-- Collapsible content -->
-        <div class="collapse navbar-collapse li-menu" id="navbarSupportedContent22">
-            <!-- Links -->
-            <ul class="navbar-nav mr-auto text-center content-menu">
-                <li class="nav-item active">
-                    <a class="nav-link" href="#">Inicio <span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item drop">
-                    <a class="nav-link dropdown-toggle" href="#">Publicaciones</a>
-                    <main class="text-center content-drop" aria-labelledby="navbarDropdownMenuLink-555">
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Noticias</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Anuncios</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Agregar Publicacion</a>
-                        </li>
-                    </main>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Plan Estrategico</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Solicitudes</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Documentos</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Usuarios</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Calendarios</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">SIG</a>
-                </li>
-            </ul>
-            <!-- Links -->
-        </div>
-        <!-- Collapsible content -->
-    </nav>
+        <?php include('../header.html') ?>
     <!--/.Navbar-->
+
+
     <div class="mt-5 sec" style="margin-top:1000px !important; ">
     </div>
+
+
+    
     <!--FOOTER -->
     <footer class="footer-login container-fluid p-0 m-0">   
         <?php include('../footer.html')?>
